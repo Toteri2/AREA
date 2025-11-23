91a354d0fe60ac1342f72dd0fb0cc54e59d3d3a7
mambokara <cbergaent@gmail.com>
2025-11-23 10:46:38 +0100

    feat App: Add start of AREA backend with db connection and a special print for the / route

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import session from 'express-session'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.use(session({
+    secret: process.env.SESSION_SECRET || 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 86400000 }
+  }))
+
+  await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
