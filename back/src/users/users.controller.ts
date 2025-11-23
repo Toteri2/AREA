6f6a738ac576fa409538ad73c12883f1b32d0390
mambokara <cbergaent@gmail.com>
2025-11-23 10:55:40 +0100

    feat users: Add complete CRUD management for users, can modify, delete, create users and get users

---
+++ back/src/users/users.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() user: {email: string, password: string}) {
+    return this.usersService.create(user.email, user.password);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() data :{ email?: string}) {
+    return this.usersService.update(id, data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.usersService.delete(id);
+  }
+}
---
+++ back/src/users/users.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm'
+import { User } from './entities/user.entity'
+import * as bcrypt from 'bcrypt'
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) { }
+
+  async create(email: string, password: string): Promise<User> {
+    const hashedPass = await bcrypt.hash(password, 10)
+    const user = this.usersRepository.create({ email: email, password: hashedPass })
+    return this.usersRepository.save(user)
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find()
+  }
+
+  async findOne(id: number): Promise<User | null> {
+    return this.usersRepository.findOneBy({ id })
+  }
+
+  async update(id: number, data: Partial<User>): Promise<User | null> {
+    await this.usersRepository.update(id, data)
+    return this.usersRepository.findOneBy({ id })
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.usersRepository.delete(id)
+  }
+
+  async checkPass(user: User, password: string): Promise<boolean> {
+    return bcrypt.compare(password, user)
+  }
+}
